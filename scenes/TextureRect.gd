bd4eec464b43c3faf4ff847bcca7c6ffad7f7850
unknown <Philipp Daher>
2019-08-31 17:38:06 +0200

    hintergrundmusik hinzugefügt

---
+++ scenes/TextureRect.gd
@@ -0,0 +1,21 @@
+func some_function(param1, param2):
+    var local_var = 5
+
+    if param1 < local_var:
+        print(param1)
+    elif param2 > 5:
+        print(param2)
+    else:
+        print("Fail!")
+
+    for i in range(20):
+        print(i)
+
+    while param2 != 0:
+        param2 -= 1
+
+    var local_var2 = param1 + 3
+    return local_var2
+	
+
+	
