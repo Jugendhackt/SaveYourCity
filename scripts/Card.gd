1374633471131f5629c050ce2fd92855ac650eca
Mastermori <mastermoriii@gmail.com>
2019-08-31 16:27:22 +0200

    Card move progress

--- scripts/Card.gd
+++ scripts/Card.gd
@@ -1,7 +1,11 @@
 extends Sprite
 
 onready var moveTween = $Tween
-onready var sprite = $Sprite
 
 func move(target):
 	moveTween.interpolate_property(self, "position", position, target, .3, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if event.is_pressed() and event.button_index == BUTTON_LEFT:
+		print("Clicked Card")
+
--- scripts/HandCards.gd
+++ scripts/HandCards.gd
@@ -4,6 +4,9 @@ var cards = []
 
 export var maxHandSize = 5
 export var padding = Vector2(5, 10)
+export var imageSize = Vector2(64, 128)
+
+var selectedCard
 
 onready var slots = get_parent().get_node("Slots")
 
@@ -12,6 +15,23 @@ func _ready():
 		cards.append(get_parent().get_node("CardStack").drawCard())
 	print(cards)
 	for i in range(0, cards.size()):
-		cards[i].position = Vector2(padding.x + (cards[i].get_texture().get_size().x + padding.x) * i, get_viewport().get_texture().get_size().y - cards[i].get_texture().get_size().y / 2 - padding.y)
+		cards[i].position = Vector2(padding.x + (imageSize.x + padding.x) * i, get_viewport().get_texture().get_size().y - imageSize.y / 2 - padding.y)
 		add_child(cards[i])
 	
+
+func positionToCard(pos):
+	var cardX = (pos.x - padding.x) / (imageSize.x + padding.x)
+	print(cardX)
+	if cardX < 0 or cardX > cards.size():
+		return null
+	else:
+		return cards[cardX]
+
+func _process(delta):
+	if Input.is_action_just_pressed("game_click"):
+		if !selectedCard:
+			selectedCard = positionToCard(get_global_mouse_position())
+			return
+		
+		if selectedCard:
+			selectedCard = null
